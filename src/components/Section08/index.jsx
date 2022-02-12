9e48988114f057e924c682aad1dd187182803d72
Leonardo Lima <leonardo.b.lima1@gmail.com>
2022-02-12 08:53:02 -0300

    secxtion08 finalizada

--- src/components/Section08/index.jsx
+++ src/components/Section08/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {ContainerStyled, WrapperStyled} from "./styles"
+import {ContainerStyled, WrapperStyled, ButtonStyled} from "./styles"
 
 
 
@@ -9,7 +9,10 @@ export const Section08 = () => {
   return (
     <ContainerStyled>
       <WrapperStyled>
-        index
+        <h1>ESSA É SUA CHANCE!</h1>
+        <h3>Garanta agora mesmo sua vaga no método completo do Ableton Live Essencial por um valor super especial.</h3>
+        <p>O método <strong>“Ableton Live Essencial”</strong> está disponível hoje por apenas 12x <strong>R$ 28,94</strong> no plano <strong>VITALÍCIO</strong>, acesso a todo conteúdo + atualizações do curso (as atualizações são feitas de acordo com a necessidade dos alunos no grupo do telegram e sempre que há atualizações no software).</p>
+        <ButtonStyled>Quero agora o ableton live essencial</ButtonStyled>
       </WrapperStyled>
     </ContainerStyled>
     )
--- src/components/Section08/styles/index.jsx
+++ src/components/Section08/styles/index.jsx
@@ -1,13 +1,12 @@
 import styled from "styled-components";
+import { darken } from "polished";
 
 
 
 
 export const ContainerStyled = styled.div`
-
-
-
-
+background-color: #00ffff;
+padding: 60px 0;
 
 
 `;
@@ -18,10 +17,95 @@ export const ContainerStyled = styled.div`
 
 
 export const WrapperStyled = styled.div`
+padding: 20px;
+max-width: 1040px;
+margin: 0 auto;
+
+h1, h3{
+  padding-bottom: 12px;
+}
+
+
+
+@media(min-width: 320px){
+  text-align: center;
+  
+  h1{
+    font-size: 26px;
+    font-weight: 800;
+  }
+  
+  h3{
+    font-size: 16px;
+    font-weight: 700;
+  }
+  
+  p{
+    font-size: 14px;
+    line-height: 22px;
+    margin-bottom: 20px;
+  }
+  
+}
+
+
+
+@media(min-width: 481px){
+  
+  h1{
+    font-size: 28px;
+  }
+  
+  h3{
+    font-size: 18px;
+    font-weight: 700;
+  }
+  
+  p{
+    font-size: 16px;
+  }
+  
+}
+
+
+
+@media(min-width: 769px){
+  
+  h1{
+    font-size: 36px;
+  }
+  
+  h3{
+    font-size: 22px;
+  }
+  
+  p{
+    font-size: 16px;
+  }
+  
+}
+
+
+
+`;
 
 
 
 
 
+export const ButtonStyled = styled.button`
 
+background-color: #289e17;
+color: white;
+text-transform: uppercase;
+border: none;
+border-radius: 5px;
+padding: 20px;
+font-size: 14px;
+font-weight: 600;
+transition: all .2s;
+
+:hover{
+  background-color: ${darken(.05,"#289e17")};
+}
 `;
